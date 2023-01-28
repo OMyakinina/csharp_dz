90344e455a71b81c3f625557a9aec9f05ab82cdb
Olga Myakinina <olga.myakinina@icloud.com>
2023-01-29 00:28:59 +0300

    add 4 programm files from home work

---
+++ EX1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает 
+// два числа и выдает, какое число большее, а какое меньшее.
+// a = 5; b = 7 ->  max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+System.Console.Write("Введите число a : ");
+int number_a = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Введите число b : ");
+int number_b = Convert.ToInt32(Console.ReadLine());
+
+if (number_a>number_b)
+{
+    System.Console.WriteLine(" Большее число: " + number_a);
+}
+else 
+System.Console.WriteLine(" Большее число: " + number_b);
---
+++ EX2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход 
+// три числа и выдает максимальное из этих чисел.
+
+System.Console.Write("Введите число a : ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Введите число b : ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Введите число c : ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a>b && a>c)
+{
+    System.Console.WriteLine("Максимальное из этих чисел : " + a);
+} 
+else if (b>a && b>c)
+{
+    System.Console.WriteLine("Максимальное из этих чисел : " + b);
+} 
+else if (c>a && c>b)
+{
+    System.Console.WriteLine("Максимальное из этих чисел : " + c);
+} 
+else 
+{
+     System.Console.WriteLine("Максимума нет");
+}
