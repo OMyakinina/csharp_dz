ad304c6b761c6a6b66f1960011e85d98c8f59063
OMyakinina <olga.myakinina@icloud.com>
2023-02-23 15:41:00 +0300

    Добавила задачу 52

---
+++ Ex52/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 52
+// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов 
+// в каждом столбце.
+
+Random array = new Random();
+int[,] arr = new int[array.Next(2, 5), array.Next(2, 5)];
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = array.Next(1, 10);
+        Console.Write(arr[i, j] + "\t ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+for (int j = 0; j < arr.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        sum += arr[i, j];
+    }
+    Console.Write($"{Math.Round( sum / arr.GetLength(0),2)} \t");
+}
+System.Console.WriteLine();
