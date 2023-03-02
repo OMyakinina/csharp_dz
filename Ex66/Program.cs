b912bef2f9f5ed5786d9e8f4ded2694be0d410ec
OMyakinina <olga.myakinina@icloud.com>
2023-03-02 12:47:11 +0300

    Добавила задачу 66

---
+++ Ex66/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+int ReadInt(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumNumbers(int m, int n)
+{
+    if (n - m + 1 == 0) return 0;
+    return SumNumbers(m, n - 1) + n;
+}
+
+var M = ReadInt("Введите число М ");
+var N = ReadInt("Введите число N ");
+Console.WriteLine(SumNumbers(M, N) + " - cумма натуральных элементов от M до N");
